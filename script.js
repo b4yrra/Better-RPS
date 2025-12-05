2ab3828641d95cafcca518d0657ddf44691e9b50
cb4yrra <t.chinbayr@gmail.com>
2025-12-05 10:25:05 +0800

    rp

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const buttonSec = document.getElementById("buttonSections");
 const footerSec = document.getElementById("footer");
 const text = document.getElementById("resultText");
 const resetBtn = document.getElementById("reset");
+const shownTexts = document.getElementById("shown");
 
 let player = 0;
 let bot = 0;
@@ -34,48 +35,52 @@ const computerChoicer = () => {
 
 const winnerCheck = (choice, computerChoice) => {
   if (choice === "rock" && computerChoice === "scissors") {
+    shownTexts.textContent = "YOU WIN";
     player++;
   }
 
   if (choice === "scissors" && computerChoice === "paper") {
+    shownTexts.textContent = "YOU WIN";
     player++;
   }
 
   if (choice === "paper" && computerChoice === "rock") {
+    shownTexts.textContent = "YOU WIN";
     player++;
   }
 
   if (choice === "rock" && computerChoice === "paper") {
+    shownTexts.textContent = "AI WIN";
     bot++;
   }
 
   if (choice === "paper" && computerChoice === "scissors") {
+    shownTexts.textContent = "AI WIN";
     bot++;
   }
 
   if (choice === "scissors" && computerChoice === "rock") {
+    shownTexts.textContent = "AI WIN";
     bot++;
   }
 
   if (choice === "rock" && computerChoice === "rock") {
-    player++;
-    bot++;
+    shownTexts.textContent = "IT'S A TIE";
   }
 
   if (choice === "paper" && computerChoice === "paper") {
-    player++;
-    bot++;
+    shownTexts.textContent = "IT'S A TIE";
   }
 
   if (choice === "scissors" && computerChoice === "scissors") {
-    player++;
-    bot++;
+    shownTexts.textContent = "IT'S A TIE";
   }
 
   playerPoint.textContent = `${player}`;
   botPoint.textContent = `${bot}`;
 
   if (player === 3 || bot === 3) {
+    shownTexts.textContent = "----";
     buttonSec.style.display = "none";
     footerSec.style.display = "flex";
 
